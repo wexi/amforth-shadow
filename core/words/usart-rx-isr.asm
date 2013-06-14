94235faf78f3b7ceffab80db7f052155f295c33b
Enoch <ixew@hotmail.com>
2013-06-14 02:17:11 -0400

    undebugged new isr tx

--- core/words/usart-rx-isr.asm
+++ core/words/usart-rx-isr.asm
@@ -1,6 +1,6 @@
 ; ( -- c)
 ; MCU
-; pause for the next usart input isr character
+; get char c from queue, pause until one arrives.
 ;; VE_RX_ISR:
 ;;     .dw $ff06
 ;;     .db "rx-isr"
@@ -10,8 +10,8 @@ XT_RX_ISR:
     .dw DO_COLON
 PFA_RX_ISR:
     .dw XT_PAUSE
-    .dw XT_RXQ_ISR		;check no of chars in queue
+    .dw XT_RXQ_ISR		;no of chars in queue
     .dw XT_DOCONDBRANCH
-    .dw PFA_RX_ISR		;no chars, pause again
+    .dw PFA_RX_ISR		;empty queue
     .dw XT_RXR_ISR		;get one char
     .dw XT_EXIT

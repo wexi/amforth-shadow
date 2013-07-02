0fdf83775f1ece6e4373b2deae723ca70a8f2bfb
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2013-07-02 18:38:30 +0000

    Documentation Fixes.
    The autodoc tools accept a single line for a short description only. any longer text
    has to go elsewhere.
    
    
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1449 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/words/wlscope.asm
+++ core/words/wlscope.asm
@@ -1,12 +1,6 @@
-; ( addr len -- wid )
+; ( addr len -- addr' len' wid )
 ; Compiler
-; wlscope, "wordlist scope" ( addr len -- addr' len' wid ), is a deferred word
-; which enables the AmForth application to choose the wordlist ( wid ) for the
-; new voc entry based on the input ( addr len ) string. The name of the new voc
-; entry ( addr' len' ) may be different from the input string. Note that all
-; created voc entry types pass through the wlscope mechanism. The default
-; wlscope action passes the input string to the output without modification and
-; uses get-current to select the wid.
+; dynamically place a word in a wordlist. The word name may be changed.
 VE_WLSCOPE:
     .dw $ff07
     .db "wlscope",0
@@ -18,3 +12,11 @@ PFA_WLSCOPE:
     .dw EE_WLSCOPE
     .dw XT_EDEFERFETCH
     .dw XT_EDEFERSTORE
+
+; wlscope, "wordlist scope" ( addr len -- addr' len' wid ), is a deferred word
+; which enables the AmForth application to choose the wordlist ( wid ) for the
+; new voc entry based on the input ( addr len ) string. The name of the new voc
+; entry ( addr' len' ) may be different from the input string. Note that all
+; created voc entry types pass through the wlscope mechanism. The default
+; wlscope action passes the input string to the output without modification and
+; uses get-current to select the wid.

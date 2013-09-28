abf152ce04d1732e5a6ff5a4cb6be665004b02ef
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2013-09-28 17:31:17 +0000

    Basic Support for the Forth201x traverse-wordlist word set.
    The proposal was acceppted at the forth 2013 meeting. 
    
    
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1453 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

---
+++ core/words/traverse-wordlist.asm
@@ -0,0 +1,60 @@
+; ( nt -- addr len ) 
+; Tools Ext (2012)
+; get a (flash) string from a name token
+VE_NAME2STRING:
+    .dw $ff0b
+    .db "name>string",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_NAME2STRING
+XT_NAME2STRING:
+    .dw DO_COLON
+PFA_NAME2STRING:
+    .dw XT_ICOUNT   ; ( -- addr n )
+    .dw XT_DOLITERAL
+    .dw $00FF
+    .dw XT_AND      ; mask immediate bit
+    .dw XT_EXIT
+
+; ( i*x xt wid -- j*x ) 
+; Tools Ext (2012)
+; iterate over a wordlist
+VE_TRAVERSEWORDLIST:
+    .dw $ff11
+    .db "traverse-wordlist",0
+    .dw VE_HEAD
+    .set VE_HEAD = VE_TRAVERSEWORDLIST
+XT_TRAVERSEWORDLIST:
+    .dw DO_COLON
+PFA_TRAVERSEWORDLIST:
+    .dw XT_FETCHE
+PFA_TRAVERSEWORDLIST1:
+    .dw XT_DUP           ; ( -- xt nt nt )
+    .dw XT_DOCONDBRANCH  ; ( -- nt ) is nfa = counted string
+    .dw PFA_TRAVERSEWORDLIST2       ;
+    .dw XT_OVER
+    .dw XT_OVER
+    .dw XT_TO_R
+    .dw XT_TO_R
+    .dw XT_SWAP
+    .dw XT_EXECUTE
+    .dw XT_R_FROM
+    .dw XT_R_FROM
+    .dw XT_ROT
+    .dw XT_DOCONDBRANCH
+    .dw PFA_TRAVERSEWORDLIST2
+    .dw XT_NFA2LFA
+    .dw XT_FETCHI        ; ( -- addr )
+    .dw XT_DOBRANCH      ; ( -- addr )
+    .dw PFA_TRAVERSEWORDLIST1       ; ( -- addr )
+PFA_TRAVERSEWORDLIST2:
+    .dw XT_DROP
+    .dw XT_DROP
+    .dw XT_EXIT
+
+; : traverse-wordlist ( i*x xt wid -- i*x' ) 
+;        begin @ dup 
+;        while 
+;          2dup 2>r 
+;          cell + swap execute ( i*x nt -- i*x' f ) 
+;          2r> rot 
+;        while repeat then 2drop ; 

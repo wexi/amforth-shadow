b289c08c520d297ff2ed0aad866aefc87179c45c
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2013-11-02 08:50:06 +0000

    Rewrite of search-wordlist using traverse-wordlist.
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1461 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/words/search-wordlist.asm
+++ core/words/search-wordlist.asm
@@ -9,54 +9,61 @@ VE_SEARCH_WORDLIST:
 XT_SEARCH_WORDLIST:
     .dw DO_COLON
 PFA_SEARCH_WORDLIST:
-    .dw XT_FETCHE
-PFA_SEARCH_WORDLIST0:
+    .dw XT_ZERO
+    .dw XT_SWAP
+    .dw XT_DOLITERAL
+    .dw XT_ISWORD
+    .dw XT_SWAP
+    .dw XT_TRAVERSEWORDLIST
     .dw XT_DUP
     .dw XT_EQUALZERO
     .dw XT_DOCONDBRANCH
     .dw PFA_SEARCH_WORDLIST1
-      ; terminating 0 found
-      .dw XT_DROP
-      .dw XT_DROP
-      .dw XT_DROP
-      .dw XT_ZERO
-      .dw XT_EXIT
+       .dw XT_DROP
+       .dw XT_DROP
+       .dw XT_DROP
+       .dw XT_ZERO
 PFA_SEARCH_WORDLIST1:
-    ; ( c-addr len searchstart )
+    .dw XT_EXIT
+
+XT_ISWORD:
+    .dw DO_COLON
+PFA_ISWORD:
+    ; ( c-addr len 0 nt -- c-addr len 0 true| xt +/-1 false )
+    .dw XT_TO_R
     .dw XT_TO_R
     .dw XT_OVER
     .dw XT_OVER
-    .dw XT_R_FETCH
-    .dw XT_ICOUNT
-    .dw XT_DOLITERAL
-    .dw $00ff
-    .dw XT_AND
-    .dw XT_ICOMPARE
+    .dw XT_R_FROM
+    .dw XT_ROT
+    .dw XT_ROT
+    .dw XT_R_FETCH  ; -- addr len 0 addr len nt
+    .dw XT_NAME2STRING
+    .dw XT_ICOMPARE      ; (-- addr len 0 f )
     .dw XT_DOCONDBRANCH
-    .dw PFA_SEARCH_WORDLISTNEXT
-      ; we found the string
+    .dw PFA_ISWORD3
+      ; we found the word, now clean up iteration data ...
       .dw XT_DROP
       .dw XT_DROP
-      .dw XT_R_FROM ; ( -- iaddr )
-      .dw XT_DUP
-      .dw XT_FETCHI
-      .dw XT_TO_R
+      .dw XT_DROP
+      ; ... get the XT ...
+      .dw XT_R_FETCH
       .dw XT_NFA2LFA
       .dw XT_1PLUS  ; lfa2xt
-      .dw XT_DOLITERAL
-      .dw 1
-      .dw XT_R_FROM ; check flags
-      .dw XT_LESSZERO ; change here for multiple flags in header
-      .dw XT_DOCONDBRANCH
-      .dw PFA_SEARCH_WORDLISTIMMEDIATE
-      .dw XT_NEGATE
-PFA_SEARCH_WORDLISTIMMEDIATE:
-      .dw XT_EXIT
-PFA_SEARCH_WORDLISTNEXT:
-      ; next try
+      ; .. and get the header flag
+      .dw XT_TRUE
       .dw XT_R_FROM
-      .dw XT_NFA2LFA
       .dw XT_FETCHI
-      .dw XT_DOBRANCH
-      .dw PFA_SEARCH_WORDLIST0
+      .dw XT_GREATERZERO
+      .dw XT_DOCONDBRANCH
+      .dw PFA_ISWORD1
+         .dw XT_NEGATE  ; it's immediate
+PFA_ISWORD1:
+      .dw XT_ZERO       ; finish traverse-wordlist
+      .dw XT_EXIT
+PFA_ISWORD3:
+    .dw XT_R_FROM
+    .dw XT_DROP
+    .dw XT_TRUE         ; maybe next word
+    .dw XT_EXIT
 

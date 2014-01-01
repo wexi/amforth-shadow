da2c33cdb70bf19ab8b5653c7f65e29ea34cffb6
Enoch <ixew@hotmail.com>
2013-12-31 20:22:13 -0500

    introduce "-! ( n a-addr -- )", a reverse action +!

--- core/words/minusstore.asm
+++ core/words/minusstore.asm
@@ -1,20 +1,20 @@
 ; ( n a-addr -- ) 
 ; Arithmetics
-; add n to content of RAM address a-addr
-VE_PLUSSTORE:
+; subtract n from the content of RAM address a-addr
+VE_MINUSSTORE:
     .dw $ff02
-    .db "+!"
+    .db "-!"
     .dw VE_HEAD
-    .set VE_HEAD = VE_PLUSSTORE
-XT_PLUSSTORE:
-    .dw PFA_PLUSSTORE
-PFA_PLUSSTORE:
+    .set VE_HEAD = VE_MINUSSTORE
+XT_MINUSSTORE:
+    .dw PFA_MINUSSTORE
+PFA_MINUSSTORE:
     movw zl, tosl		;a-addr
     loadtos			;n
     ldd temp0, Z+0
     ldd temp1, Z+1
-    add temp0, tosl
-    adc temp1, tosh
+    sub temp0, tosl
+    sbc temp1, tosh
     std Z+0, temp0
     std Z+1, temp1
     loadtos

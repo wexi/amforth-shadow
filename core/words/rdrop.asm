893193ce1da17c2eaf0efcfe927eea8c5f26492c
Enoch <ixew@hotmail.com>
2014-01-19 22:24:08 -0500

    2rdrop

--- core/words/rdrop.asm
+++ core/words/rdrop.asm
@@ -1,6 +1,6 @@
-; (R: n --)
+; rdrop ( R: n --), 2rdrop ( R: n1 n2 --)
 ; Stack
-; drop TOR
+; drop TOR, drop two TORs
 VE_RDROP:
     .dw $ff05
     .db "rdrop",0
@@ -8,6 +8,18 @@ VE_RDROP:
     .set VE_HEAD = VE_RDROP
 XT_RDROP:
     .dw PFA_RDROP
+;
+VE_2RDROP:
+    .dw $ff06
+    .db "2rdrop"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_2RDROP
+XT_2RDROP:
+    .dw PFA_2RDROP
+;
+PFA_2RDROP:
+    pop temp2
+    pop temp3
 PFA_RDROP:
     pop temp0
     pop temp1

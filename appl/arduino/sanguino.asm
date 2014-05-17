90a4f178c37458c3d18fbe11bcf83d3db6be6262
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-05-17 11:49:11 +0000

    Consolidate option settings into a master file, while retaining user changes in the application master file.
    Greatly simplifies default settings. Based on ideas from Paolo.
    
    
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1584 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- appl/arduino/sanguino.asm
+++ appl/arduino/sanguino.asm
@@ -1,24 +1,13 @@
-; Settings for the eval board with Atmega8 & 8 MHz
+; for a description, what can be done in this
+; file see ../template/template.asm. You may want to
+; copy that file to this one and edit it afterwards.
+
 .include "macros.asm"
 .include "device.asm"
 
 .equ AMFORTH_RO_SEG = NRWW_START_ADDR
 
-.equ TIBSIZE  = $64    ; 80 characters is one line
-.equ APPUSERSIZE = 10  ; size of application specific user area
-
-; cpu clock in hertz
 .equ F_CPU = 16000000
-; baud rate of terminal
-
-.equ BAUD = 9600
 .include "drivers/usart_0.asm"
-.set rstackstart = RAMEND
-.set stackstart  = RAMEND - 80
-
-.set NUMWORDLISTS = 8
-
-; some registers
-.set WANT_PORTA = 0
 
 .include "amforth.asm"
--- appl/atxmega/atxmega.asm
+++ appl/atxmega/atxmega.asm
@@ -2,18 +2,10 @@
 .include "macros.asm"
 .include "device.asm"
 
-.equ TIBSIZE  = $64    ; 80 characters is one line...
-.equ APPUSERSIZE = 10  ; size of user area
-.set stackstart  = RAMEND - 80
-.set rstackstart = RAMEND
-
-
-.equ NUMWORDLISTS = 8
-
 ; cpu clock in hertz
 .equ F_CPU = 2000000
 ; baud rate of terminal
-.equ BAUD = 9600
+.set BAUD = 9600
 .equ TERM_USART = USARTE0_base
 .equ TERM_USART_PORT = PORTE_base
 .include "drivers/usartx.asm"

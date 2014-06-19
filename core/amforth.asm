5f34993cd5fb857b5c818ea047630d9d37f06d43
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-06-19 15:05:14 +0000

    removed duplicate label, segment boundary check at compile time added
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1611 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/amforth.asm
+++ core/amforth.asm
@@ -4,9 +4,21 @@
 
 .set AMFORTH_NRWW_SIZE=(FLASHEND-AMFORTH_RO_SEG)*2
 
-.set pc_ = pc
+.set corepc = pc
 .org $0000
   jmp_ PFA_COLD
+
+.org corepc
+.include "drivers/generic-isr.asm"
+; lower part of the dictionary
+.include "dict/rww.inc"
+.include "dict_appl.inc"
+
+.set DPSTART = pc
+.if(pc>AMFORTH_RO_SEG)
+.error "RWW Segment Overflow, please edit your dict_appl.inc"
+.endif
+
 .org AMFORTH_RO_SEG
 .include "amforth-interpreter.asm"
 .include "dict/nrww.inc"
@@ -16,15 +28,6 @@
 .if (pc>FLASHEND)
   .error "*** Flash size exceeded, please edit your dict_appl_core file to use less space! Aborting."
 .endif
-
-.org pc_
-.include "drivers/generic-isr.asm"
-; lower part of the dictionary
-.include "dict/rww.inc"
-.include "dict_appl.inc"
-
-.set DPSTART = pc
-
 
 .dseg
 ; define a label for the 1st free ram address

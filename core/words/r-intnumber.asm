565eb91ef4da520e2b32a22da89c07ab84ac9391
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-06-26 19:02:57 +0000

    Renamed to follow RFD Draft
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1619 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/words/r-intnumber.asm
+++ core/words/r-intnumber.asm
@@ -1,14 +1,14 @@
 ; ( -- addr )
 ; Interpreter
 ; Method table for single cell integers
-VE_R_INTNUM:
-    .dw $ff08
-    .db "r:intnum"
+VE_R_NUM:
+    .dw $ff05
+    .db "r:num",0
     .dw VE_HEAD
-    .set VE_HEAD = VE_R_INTNUM
-XT_R_INTNUM:
+    .set VE_HEAD = VE_R_NUM
+XT_R_NUM:
     .dw PFA_DOCONSTANT
-PFA_R_INTNUM:
+PFA_R_NUM:
     .dw XT_NOOP    ; interpret
     .dw XT_LITERAL ; compile
     .dw XT_FAILNUM ; postpone
@@ -16,14 +16,14 @@ PFA_R_INTNUM:
 ; ( -- addr )
 ; Interpreter
 ; Method table for double cell integers
-VE_R_INTDNUM:
-    .dw $ff09
-    .db "r:intdnum",0
+VE_R_DNUM:
+    .dw $ff06
+    .db "r:dnum"
     .dw VE_HEAD
-    .set VE_HEAD = VE_R_INTDNUM
-XT_R_INTDNUM:
+    .set VE_HEAD = VE_R_DNUM
+XT_R_DNUM:
     .dw PFA_DOCONSTANT
-PFA_R_INTDNUM:
+PFA_R_DNUM:
     .dw XT_NOOP     ; interpret
     .dw XT_2LITERAL ; compile
     .dw XT_FAILDNUM ; postpone
@@ -65,14 +65,14 @@ PFA_FAILDNUM:
 ; ( addr len -- f )
 ; Interpreter
 ; recognizer for integer numbers
-VE_REC_INTNUM:
-    .dw $ff0a
-    .db "rec:intnum"
+VE_REC_NUM:
+    .dw $ff07
+    .db "rec:num",0
     .dw VE_HEAD
-    .set VE_HEAD = VE_REC_INTNUM
-XT_REC_INTNUM:
+    .set VE_HEAD = VE_REC_NUM
+XT_REC_NUM:
     .dw DO_COLON
-PFA_REC_INTNUM:
+PFA_REC_NUM:
     ; try converting to a number
     .dw XT_NUMBER
     .dw XT_DOCONDBRANCH
@@ -82,10 +82,10 @@ PFA_REC_INTNUM:
     .dw XT_EQUAL
     .dw XT_DOCONDBRANCH
     .dw PFA_REC_INTNUM2
-      .dw XT_R_INTNUM
+      .dw XT_R_NUM
       .dw XT_EXIT
 PFA_REC_INTNUM2:
-      .dw XT_R_INTDNUM
+      .dw XT_R_DNUM
       .dw XT_EXIT
 PFA_REC_NONUMBER:
     .dw XT_R_FAIL

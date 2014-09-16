575af803c82575c80b26cd2a00f4a29c19e6ebaf
Enoch <ixew@hotmail.com>
2014-09-16 14:10:06 -0400

    suspected um* so rewritten but that's not the case...

--- core/words/umstar.asm
+++ core/words/umstar.asm
@@ -1,4 +1,4 @@
-; ( u1 u2 -- d) 
+; ( u1 u2 -- ud) 
 ; Arithmetics
 ; multiply 2 unsigned cells to a double cell
 VE_UMSTAR:
@@ -9,29 +9,23 @@ VE_UMSTAR:
 XT_UMSTAR:
     .dw PFA_UMSTAR
 PFA_UMSTAR:
-    movw temp0, tosl
-    loadtos
-    ; result: (temp3*temp1)* 65536 + (temp3*temp0 + temp1*temp2) * 256 + (temp0 * temp2)
-    ; low bytes
-    mul tosl,temp0
-    movw zl, r0
-    clr temp2
-    clr temp3
-    ; middle bytes
-    mul tosh, temp0
-    add zh, r0
-    adc temp2, r1
-    adc temp3, zeroh
-        
-    mul tosl, temp1
-    add zh, r0
-    adc temp2, r1
-    adc temp3, zeroh
-    
-    mul tosh, temp1
-    add temp2, r0
-    adc temp3, r1
-    movw tosl, zl
-    savetos
-    movw tosl, temp2
+    ldd	temp0,y+0
+    ldd	temp1,y+1
+    mul	temp0,tosl
+    std	y+0, r0
+    mov	temp2, r1
+    mul	temp1,tosh
+    mov	temp3, r0
+    mov	temp4, r1
+    mul	temp1,tosl
+    add	temp2, r0
+    adc	temp3, r1
+    adc	temp4, zerol
+    mul	temp0, tosh
+    add	temp2, r0
+    adc	temp3, r1
+    adc	temp4, zerol
+    std	y+1, temp2
+    mov	tosl, temp3
+    mov	tosh, temp4
     jmp_ DO_NEXT

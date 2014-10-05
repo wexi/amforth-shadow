c20224e1482059af6574a2aeff31d65db59932ee
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2014-10-05 08:14:24 +0000

    cpu frequency cannot be changed at runtime
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1672 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/words/f_cpu.asm
+++ core/words/f_cpu.asm
@@ -7,28 +7,10 @@ VE_F_CPU:
     .dw VE_HEAD
     .set VE_HEAD = VE_F_CPU
 XT_F_CPU:
-    .dw PFA_DOVALUE1
+    .dw DO_COLON
 PFA_F_CPU:
-    .dw EE_FCPU
-    .dw XT_FETCH2E
-    .dw XT_STORE2E
-
-XT_FETCH2E:
-    .dw DO_COLON
-PFA_FETCH2E:
-    .dw XT_FETCHI
-    .dw XT_DUP
-    .dw XT_CELLPLUS
-    .dw XT_FETCHE
-    .dw XT_SWAP
-    .dw XT_FETCHE
-    .dw XT_SWAP
-    .dw XT_EXIT
-
-XT_STORE2E:
-    .dw DO_COLON
-PFA_STORE2E:
     .dw XT_DOLITERAL
-    .dw -21
-    .dw XT_THROW
-
+    .dw (F_CPU % 65536)
+    .dw XT_DOLITERAL
+    .dw (F_CPU / 65536)
+    .dw XT_EXIT

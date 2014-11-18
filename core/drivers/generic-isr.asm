5a8bda26bde5c88dc6aa8907fdf34fb9250a10db
Enoch <ixew@hotmail.com>
2014-11-18 01:05:52 -0500

    SPI interrupts handling revised.

--- core/drivers/generic-isr.asm
+++ core/drivers/generic-isr.asm
@@ -3,21 +3,61 @@
 #ifdef	INTQUE
 .equ intsiz = INTQUE
 #else
-.equ intsiz = 8			;queue size
+.equ intsiz = 8			;interrupts queue length
 #endif
 
+; Note:
+	
+; Clear hard interrupts overflow by: 0 int' c!
+; Enclose critical sections by: int- ... int+
+; Serve soft interrupts from critical section: INT*
+	
+; SPI / CAN interrupts auto disable
+
 .dseg
-svTEMP:	.byte 1			;saves TEMP0
-svSREG:	.byte 1			;saves SREG
 intovf:	.byte 1			;int'→ lo: hard interrupts overflow (nz = prog addr)
 intswi:	.byte 1			;int'→ hi: soft interrupts inhibit  (nz = inhibited)
+oSPDR:	.byte 1			;SPDR output buffer
+iSPDR:	.byte 1			;SPDR input buffer
 intbuf:	.byte intsiz+1		;last byte always zero
 intvec:	.byte INTVECTORS * CELLSIZE
 
-; clear hard interrupts overflow mark: 0 int' c!
-; enable/disable soft interrupts by:   int+ int-
-	
 .cseg
+VE_oSPDR:
+	.dw	$ff05
+	.db	"oSPDR",0
+	.dw	VE_HEAD
+	.set	VE_HEAD = VE_oSPDR
+XT_oSPDR:
+	.dw	PFA_DOVARIABLE
+PFA_oSPDR:
+	.dw	oSPDR
+
+VE_iSPDR:
+	.dw	$ff05
+	.db	"iSPDR",0
+	.dw	VE_HEAD
+	.set	VE_HEAD = VE_iSPDR
+XT_iSPDR:
+	.dw	PFA_DOVARIABLE
+PFA_iSPDR:
+	.dw	iSPDR
+
+.set	pc_ = pc
+.org	SPIaddr
+	rcall	isrspi
+.org	pc_
+
+isrspi:	st	-Y, temp0	;SPIF cleared by interrupt
+	in_	temp0, SREG
+	st	-Y, temp1
+	in_	temp1, SPDR	;buffer i/o
+	sts	iSPDR, temp1
+	lds	temp1, oSPDR
+	out_	SPDR, temp1
+	cbi_	SPCR, SPIE, temp1 ;disable SPI interrupts
+	rjmp	isrcom
+	
 #ifdef	_CAN32DEF_INC_
 #define	_CANDEF_
 #endif
@@ -31,72 +71,54 @@ intvec:	.byte INTVECTORS * CELLSIZE
 #ifdef	_CANDEF_
 .set	pc_ = pc
 .org	CANITaddr
-	rjmp	can_isr
+	rcall	isrcan
 .org	pc_
-can_isr:			;since CAN interrupts are not ack'ed by handler exec
-	sts	svTEMP, TEMP0
-	in	TEMP0, SREG
-	sts	svSREG, TEMP0
-	lds	TEMP0, CANGIE	;disable all CAN interrupts
-	andi	TEMP0, $7F
-	sts	CANGIE, TEMP0
-	ldi	TEMP0, CANITaddr ;interrupt ID
-	rjmp	isr_join  
-#endif	
-
-; interrupt routine gets called (again) by rcall! This gives the
-; address of the int-vector on the stack.
-isr:
-	sts	svTEMP, TEMP0
-	in	TEMP0, SREG
-	sts	svSREG, TEMP0
-
-.if 	pclen == 3
-	pop	TEMP0		;some 128+K Flash devices use 3 cells for call/ret
-.endif
-	pop	TEMP0
-	pop	TEMP0		;= intnum * intvectorsize + 1 (address following the rcall)
-	dec	TEMP0
-.if 	intvecsize == 1
-	lsl	TEMP0
-.endif
 	
-isr_join:			;TEMP0 = interrupt address
-	push	TEMP1
-	push	ZL
-	push	ZH
+isrcan:	st	-Y, temp0	;CANIT not cleared by interrupt
+	in_	temp0, SREG
+	st	-Y, temp1
+	cbi_	CANGIE, ENIT, temp1 ;disable CAN interrupts
+	rjmp	isrcom  
+#endif	
+	
+isr:	st	-Y, temp0
+	in_	temp0, SREG
+	st	-Y, temp1
+	
+isrcom:	pop	temp1		;rcall passes faddr+1
+	pop	temp1		;big endian stored
+	dec	temp1		;interrupt vector address
+	
+	st	-Y, temp2
+	st	-Y, zl
+	st	-Y, zh
 	ldiw	Z, intbuf
 	
 ; crude yet efficient queue (input) if having low occupancy
 	
 .macro	inp_buf
 .if	@0
-	ld	TEMP1, Z+
-	tst	TEMP1
-	breq	inp_cur		;free Q place?
+	ld	temp2, Z+
+	tst	temp2
+	breq	isrsav		;free Q place?
 	inp_buf	(@0-1)
 .endif
 .endmacro
 
 	inp_buf intsiz
 	
-	sts	intovf, TEMP0	;mark overflow with prog addr
-	lds	TEMP0, svSREG
-	rjmp	int_swi
+	sts	intovf, temp1	;mark overflow with prog addr
+	rjmp	isrbye
 	
-inp_cur:
-	st	-Z, TEMP0	;save interrupt address in queue
+isrsav:	st	-Z, temp1	;save interrupt address in queue
+	lds	temp1, intswi
+	sbrs	temp1, 7	;skip if interrupts off
+	sbr	temp0, exp2(SREG_T) ;T bit position
 
-	lds	TEMP0, intswi
-	tst	TEMP0
-	lds	TEMP0, svSREG	;TEMP0 = saved SREG
-	brne	int_swi		;soft interrupts inhibited?
-	ori	TEMP0, $40	;set T bit pos to interrupt forth
-	
-int_swi:
-	pop	ZH
-	pop	ZL
-	pop	TEMP1
-	out	SREG, TEMP0
-	lds	TEMP0, svTEMP
+isrbye:	ld	zh, Y+
+	ld	zl, Y+
+	ld	temp2, Y+
+	ld	temp1, Y+
+	out_	SREG, temp0
+	ld	temp0, Y+
 	reti

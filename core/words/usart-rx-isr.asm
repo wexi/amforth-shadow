5c644e1267e77d54ef7b4b51d932bfd8cc9274ea
Enoch <ixew@hotmail.com>
2014-12-19 13:44:31 -0500

    Serial port queue privileged access

--- core/words/usart-rx-isr.asm
+++ core/words/usart-rx-isr.asm
@@ -9,12 +9,15 @@ VE_RX_ISR:
 XT_RX_ISR:
     .dw DO_COLON
 PFA_RX_ISR:
+    .dw XT_SWIDI		;int-
     .dw XT_RXQ_ISR		;no of chars in queue
     .dw XT_DOCONDBRANCH
     .dw	PFA_RX_ISR1		;empty queue
+    .dw XT_SWIEN		;int+
     .dw XT_RXR_ISR		;get one char
     .dw XT_EXIT
 PFA_RX_ISR1:
+    .dw XT_SWIEN		;int+
     .dw XT_PAUSE
     .dw XT_DOBRANCH
     .dw PFA_RX_ISR
--- core/words/usart-tx-isr.asm
+++ core/words/usart-tx-isr.asm
@@ -9,6 +9,7 @@ VE_TX_ISR:
 XT_TX_ISR:
     .dw	DO_COLON
 PFA_TX_ISR:
+    .dw XT_SWIDI		;int-
     .dw	XT_TXQ_ISR		;no of available char places
     .dw	XT_QDUP			;?dup
     .dw	XT_DOCONDBRANCH
@@ -16,12 +17,15 @@ PFA_TX_ISR:
     .dw	XT_GREATERZERO		;0>
     .dw	XT_DOCONDBRANCH
     .dw	PFA_TX_ISR2		;DTR is OFF
+    .dw XT_SWIEN		;int+
     .dw	XT_TXR_ISR		;putch
     .dw	XT_EXIT
 PFA_TX_ISR1:
+    .dw XT_SWIEN		;int+
     .dw	XT_PAUSE
     .dw XT_DOBRANCH
     .dw PFA_TX_ISR 
 PFA_TX_ISR2:
+    .dw XT_SWIEN		;int+
     .dw	XT_DROP
     .dw	XT_EXIT

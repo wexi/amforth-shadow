9c69cdef3e820847813749f79d5793cd3a19163a
Enoch <ixew@hotmail.com>
2015-05-19 15:36:40 -0400

    Adds "previous", "also" reverse action.

--- core/words/also.asm
+++ core/words/also.asm
@@ -21,7 +21,32 @@ PFA_ALSO:
     .dw XT_SET_ORDER
     .dw XT_EXIT
 PFA_ALSO1:
-; -32: invalid name argument
     .dw XT_DOLITERAL
     .dw -32
-    .dw XT_THROW
+    .dw XT_THROW		;invalid name argument
+
+; ( -- ) 
+; Search Order
+; Remove search order top
+VE_PREVIOUS:
+    .dw $ff08
+    .db "previous"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_PREVIOUS
+XT_PREVIOUS:
+    .dw DO_COLON
+PFA_PREVIOUS:
+    .dw XT_GET_ORDER
+    .dw XT_DUP	
+    .dw XT_ONE
+    .dw XT_UGREATER
+    .dw XT_DOCONDBRANCH	
+    .dw PFA_PREVIOUS1
+    .dw XT_NIP
+    .dw XT_1MINUS
+    .dw XT_SET_ORDER
+    .dw XT_EXIT
+PFA_PREVIOUS1:
+    .dw XT_DOLITERAL
+    .dw -50
+    .dw XT_THROW		;search-order underflow

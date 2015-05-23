c522c6a526785a0f5870f71a0fc7d49db3a200f4
Enoch <ixew@hotmail.com>
2015-05-23 14:04:36 -0400

    USART ISR driver trivial changes before a big one...

--- core/drivers/usart-isr-rx.asm
+++ core/drivers/usart-isr-rx.asm
@@ -1,117 +1,129 @@
 ;;; usart receiver interrupt service
 
-.set pc_ = pc
-.org URXCaddr
-  jmp_ usart_rx_isr
-.org pc_
+	.set pc_ = pc
+	.org URXCaddr
+	jmp_ usart_rx_isr
+.	org pc_
 
 #ifdef RXR_SIZE
-.equ usart_rx_siz = RXR_SIZE	;must be a power of 2
+	.equ 	USART_RX_SIZ = RXR_SIZE	;must be a power of 2
 #else
-.equ usart_rx_siz = 16
+	.equ USART_RX_SIZ = 16
 #endif
-.equ usart_rx_msk = usart_rx_siz - 1
-.equ usart_rx_off = usart_rx_siz - 2 ;≤ cnt: CTS_OFF 
-.equ usart_rx_onn = usart_rx_siz / 2 ;≥ cnt: CTS_ON  
+	.equ USART_RX_MSK = USART_RX_SIZ - 1
+	.equ USART_RX_OFF = USART_RX_SIZ - 2 ;≤ cnt: CTS_OFF 
+	.equ USART_RX_ONN = USART_RX_SIZ / 2 ;≥ cnt: CTS_ON  
 	
-.dseg
-usart_rx_inp: .byte 1
-usart_rx_out: .byte 1
-usart_rx_cnt: .byte 1
-usart_rx_dat: .byte usart_rx_siz
+	.IF	USART_RX_SIZ > 256
+	.MESSAGE 256 is largest buffer
+	.ENDIF
+	
+	.dseg
+usart_rx_inp:
+	.byte 1
+usart_rx_out:
+	.byte 1
+usart_rx_cnt:
+	.byte 1
+usart_rx_dat:
+	.byte USART_RX_SIZ
 
-.cseg
+	.cseg
 usart_rx_isr:
-  push 	xl
-  in 	xl, SREG
-  push 	xl
-  push 	xh
-  push 	zl
-  push 	zh
+	push 	xl
+	in 	xl, SREG
+	push 	xl
+	push 	xh
+	push 	zl
+	push 	zh
 
-  lds	xh, USART_DATA		;clears interrupt
-  lds	xl, usart_rx_cnt
+	lds	xh, USART_DATA	;clears interrupt
+	lds	xl, usart_rx_cnt
 
 #ifdef	CTS_ENABLE
-  cpi	xl, usart_rx_off - 1
-  brlo	usart_rx_isr_store
-  CTS_OFF
+	cpi	xl, USART_RX_OFF - 1
+	brlo	usart_rx_isr_store
+	CTS_OFF
 #endif
 
 usart_rx_isr_store:
-  cpi	xl, usart_rx_siz
-  brsh	usart_rx_isr_done	;overflow?
-  inc	xl
-  sts	usart_rx_cnt, xl
+	cpi	xl, USART_RX_SIZ
+	brsh	usart_rx_isr_done ;overflow?
+	inc	xl
+	sts	usart_rx_cnt, xl
 	
-  lds	xl, usart_rx_inp
-  ldiw	z, usart_rx_dat
-  add	zl, xl
-  adc	zh, zeroh
-  st	z, xh
-  inc	xl
-  andi	xl, usart_rx_msk
-  sts	usart_rx_inp, xl
+	lds	xl, usart_rx_inp
+	ldiw	z, usart_rx_dat
+	add	zl, xl
+	adc	zh, zeroh
+	st	z, xh
+	inc	xl
+	.IF	USART_RX_MSK < 255
+	andi	xl, USART_RX_MSK
+	.ENDIF
+	sts	usart_rx_inp, xl
 
 usart_rx_isr_done:
-  pop	zh
-  pop	zl
-  pop	xh
-  pop	xl
-  out	SREG, xl
-  pop	xl
-  reti
-
+	pop	zh
+	pop	zl
+	pop	xh
+	pop	xl
+	out	SREG, xl
+	pop	xl
+	reti
+	
 ; ( -- cnt ) number of characters in queue
 VE_RXQ_ISR:
-    .dw $ff07
-    .db "rxq-isr",0
-    .dw VE_HEAD
-    .set VE_HEAD = VE_RXQ_ISR
+	.dw 	$ff07
+	.db 	"rxq-isr",0
+	.dw 	VE_HEAD
+	.set 	VE_HEAD = VE_RXQ_ISR
 XT_RXQ_ISR: _pfa_
-  savetos
-  lds	tosl, usart_rx_cnt
-  clr	tosh
+	savetos
+	lds	tosl, usart_rx_cnt
+	clr	tosh
 #ifdef	CTS_ENABLE
-  IS_CTS_OFF
-  rjmp	RXQ_ISR			;already ON 
-  cpi	tosl, usart_rx_onn + 1
-  brsh	RXQ_ISR
-  CTS_ON			;cnt ≤ usart_rx_onn
+	IS_CTS_OFF
+	rjmp	RXQ_ISR		;already ON 
+	cpi	tosl, USART_RX_ONN + 1
+	brsh	RXQ_ISR
+	CTS_ON			;cnt ≤ USART_RX_ONN
 #endif
 RXQ_ISR:
-  jmp_	DO_NEXT
+	jmp_	DO_NEXT
 	
 ; ( -- c ) assuming the queue is not empty!
 XT_RXR_ISR: _pfa_
-  savetos
-  lds	temp0, usart_rx_out
-  ldiw	z, usart_rx_dat
-  add 	zl, temp0
-  adc	zh, zeroh
-  ld	tosl, z
-  clr	tosh
-  inc	temp0
-  andi	temp0, usart_rx_msk
-  sts	usart_rx_out, temp0
+	savetos
+	lds	temp0, usart_rx_out
+	ldiw	z, usart_rx_dat
+	add 	zl, temp0
+	adc	zh, zeroh
+	ld	tosl, z
+	clr	tosh
+	inc	temp0
+	.IF	USART_RX_MSK < 255
+	andi	temp0, USART_RX_MSK
+	.ENDIF
+	sts	usart_rx_out, temp0
 
-  in	temp0, SREG		;uninterruptible count dec
-  cli
-  lds	temp1, usart_rx_cnt
-  dec	temp1
-  sts	usart_rx_cnt, temp1
-  out	SREG, temp0  
-  jmp_	DO_NEXT
+	in	temp0, SREG	;uninterruptible count dec
+	cli
+	lds	temp1, usart_rx_cnt
+	dec	temp1
+	sts	usart_rx_cnt, temp1
+	out	SREG, temp0  
+	jmp_	DO_NEXT
 
 ; called by +usart
 XT_USART_INIT_RX_ISR: _pfa_
-  in	temp0, SREG
-  cli
-  sts	usart_rx_inp, zerol
-  sts	usart_rx_out, zerol
-  sts	usart_rx_cnt, zerol
-  out	SREG, temp0  
+	in	temp0, SREG
+	cli
+	sts	usart_rx_inp, zerol
+	sts	usart_rx_out, zerol
+	sts	usart_rx_cnt, zerol
+	out	SREG, temp0  
 #ifdef	CTS_ENABLE
-  CTS_INIT
+	CTS_INIT
 #endif
-  jmp_	DO_NEXT	
+	jmp_	DO_NEXT	
--- core/drivers/usart-isr-tx.asm
+++ core/drivers/usart-isr-tx.asm
@@ -1,152 +1,164 @@
 ;;; usart transmitter interrupt service
 
-.set pc_ = pc
-.org UDREaddr
-  jmp_ usart_tx_isr
-.org pc_
+	.set 	pc_ = pc
+	.org 	UDREaddr
+	jmp_ 	usart_tx_isr
+	.org 	pc_
 
 #ifdef TXR_SIZE
-.equ usart_tx_siz = TXR_SIZE	;must be a power of 2
+	.equ 	USART_TX_SIZ = TXR_SIZE	;power of 2 and less than 257
 #else
-.equ usart_tx_siz = 16
+	.equ 	USART_TX_SIZ = 16
 #endif
-.equ usart_tx_msk = usart_tx_siz - 1
+	.equ 	USART_TX_MSK = USART_TX_SIZ - 1
+
+	.IF	USART_TX_SIZ > 256
+	.MESSAGE 256 is largest buffer
+	.ENDIF
 	
-.dseg
-usart_tx_inp: .byte 1
-usart_tx_out: .byte 1
-usart_tx_cnt: .byte 1
-usart_tx_dat: .byte usart_tx_siz
+	.dseg
+usart_tx_inp:
+	.byte 1
+usart_tx_out:
+	.byte 1
+usart_tx_cnt:
+	.byte 1
+usart_tx_dat:
+	.byte 	USART_TX_SIZ
 
-.cseg
+	.cseg
 usart_tx_isr:
-  push 	xl
-  in 	xl, SREG
-  push 	xl
-  push 	zl
-  push 	zh
+	push	xl
+	in 	xl, SREG
+	push 	xl
+	push 	zl
+	push 	zh
 
-  lds	xl, usart_tx_cnt
-  dec	xl
-  sts	usart_tx_cnt, xl
+	lds	xl, usart_tx_cnt
+	dec	xl
+	sts	usart_tx_cnt, xl
 	
 #ifdef	RTS_ENABLE
-  IS_RTS_OFF
+	IS_RTS_OFF
 #endif
-  brne	usart_udre_next
+	brne	usart_udre_next
 
 usart_udre_halt:
-  lds	xl, USART_B		;disable transmit empty interrupts
-  cbr	xl, bm_ENABLE_INT_TX
-  sts	USART_B, xl
-  rjmp usart_udre_exit
+	lds	xl, USART_B		;disable transmit empty interrupts
+	cbr	xl, bm_ENABLE_INT_TX
+	sts	USART_B, xl
+	rjmp 	usart_udre_exit
 
 #ifdef	RTS_ENABLE
 usart_udre_resume:
-  lds xl, USART_B
-  sbr xl, bm_ENABLE_INT_TX
-  sts USART_B, xl
+	lds 	xl, USART_B
+	sbr 	xl, bm_ENABLE_INT_TX
+	sts 	USART_B, xl
 #endif
 
 usart_udre_next:
-  lds	xl, usart_tx_out
-  ldiw	z, usart_tx_dat
-  add	zl, xl
-  adc	zh, zeroh
-  inc	xl
-  andi	xl, usart_tx_msk
-  sts	usart_tx_out, xl
-  ld	xl, z
-  sts	USART_DATA, xl
+	lds	xl, usart_tx_out
+	ldiw	z, usart_tx_dat
+	add	zl, xl
+	adc	zh, zeroh
+	inc	xl
+	.IF	USART_TX_MSK < 255
+	andi	xl, USART_TX_MSK
+	.ENDIF
+	sts	usart_tx_out, xl
+	ld	xl, z
+	sts	USART_DATA, xl
 
 usart_udre_exit:
-  pop zh
-  pop zl
-  pop xl
-  out SREG,xl
-  pop xl
-  reti
+	pop 	zh
+	pop 	zl
+	pop 	xl
+	out 	SREG,xl
+	pop 	xl
+	reti
 
 #ifdef	RTS_ENABLE
 usart_rts_isr:			;RTS OFF→ON
-  push 	xl
-  in 	xl, SREG
-  push 	xl
-  push 	zl
-  push 	zh
+	push 	xl
+	in 	xl, SREG
+	push 	xl
+	push 	zl
+	push 	zh
 
-  lds	xl, usart_tx_cnt
-  tst	xl
-  brne	usart_udre_resume
-  rjmp	usart_udre_exit
+	lds	xl, usart_tx_cnt
+	tst	xl
+	brne	usart_udre_resume
+	rjmp	usart_udre_exit
 #endif	
 
 ; ( -- n ) return the number of char places in queue or -1 if the DTR is OFF
 VE_TXQ_ISR:
-    .dw $ff07
-    .db "txq-isr",0
-    .dw VE_HEAD
-    .set VE_HEAD = VE_TXQ_ISR
+	.dw 	$ff07
+	.db 	"txq-isr",0
+	.dw 	VE_HEAD
+	.set 	VE_HEAD = VE_TXQ_ISR
 XT_TXQ_ISR: _pfa_
-  savetos
+	savetos
 #ifdef	DTR_ENABLE
-  IS_DTR_OFF
-  rjmp	TXQ_ISR
-  ldiw	tos, -1
-  jmp_	DO_NEXT
+	IS_DTR_OFF
+	rjmp	TXQ_ISR
+	ldiw	tos, -1
+	jmp_	DO_NEXT
 #endif
 TXQ_ISR:	
-  lds	temp0, usart_tx_cnt
-  ldi	tosl, usart_tx_siz
-  sub	tosl, temp0
-  clr	tosh
-  jmp_	DO_NEXT
+	lds	temp0, usart_tx_cnt
+	ldi	tosl, USART_TX_SIZ
+	sub	tosl, temp0
+	clr	tosh
+	jmp_	DO_NEXT
 
 ; ( c -- ) assuming the queue is not full!
 XT_TXR_ISR: _pfa_
-  lds	temp0, usart_tx_inp	;insert c to queue
-  ldiw	z, usart_tx_dat
-  add 	zl, temp0
-  adc	zh, zeroh
-  st	z, tosl
-  loadtos
-  inc	temp0
-  andi	temp0, usart_tx_msk
-  sts	usart_tx_inp, temp0
+	lds	temp0, usart_tx_inp	;insert c to queue
+	ldiw	z, usart_tx_dat
+	add 	zl, temp0
+	adc	zh, zeroh
+	st	z, tosl
+	loadtos
+	inc	temp0
+	.IF	USART_TX_MSK < 255
+	andi	temp0, USART_TX_MSK
+	.ENDIF
+	sts	usart_tx_inp, temp0
 
-  in	temp0, SREG
-  cli
-  lds	temp1, usart_tx_cnt
-  tst	temp1
+	in	temp0, SREG
+	cli
+	lds	temp1, usart_tx_cnt
+	tst	temp1
 #ifdef	RTS_ENABLE
-  IS_RTS_OFF
+	IS_RTS_OFF
 #endif
-  breq	TXR_ISR1		;first queue insertion?
-  rjmp	TXR_ISR2		;active tx or RTS is OFF
+	breq	TXR_ISR1		;first queue insertion?
+	rjmp	TXR_ISR2		;active tx or RTS is OFF
 TXR_ISR1:
-  lds temp2, USART_B
-  sbr temp2, bm_ENABLE_INT_TX
-  sts USART_B, temp2		;force tx empty interrupt
-  inc	temp1			;account for ghost char tx
+	lds 	temp2, USART_B
+	sbr 	temp2, bm_ENABLE_INT_TX
+	sts 	USART_B, temp2		;force tx empty interrupt
+	inc	temp1			;account for ghost char tx
 TXR_ISR2:
-  inc	temp1
-  sts	usart_tx_cnt, temp1
-  out	SREG, temp0  
-  jmp_	DO_NEXT
+	inc	temp1
+	sts	usart_tx_cnt, temp1
+	out	SREG, temp0  
+	jmp_	DO_NEXT
 
 ; called by +usart
 XT_USART_INIT_TX_ISR: _pfa_
-  in	temp0, SREG
-  cli
-  sts	usart_tx_inp, zerol
-  sts	usart_tx_out, zerol
-  sts	usart_tx_cnt, zerol
-  lds	temp1, USART_B		;disable transmit empty interrupts
-  cbr	temp1, bm_ENABLE_INT_TX
-  sts	USART_B, temp1
-  out	SREG, temp0  
+	in	temp0, SREG
+	cli
+	sts	usart_tx_inp, zerol
+	sts	usart_tx_out, zerol
+	sts	usart_tx_cnt, zerol
+	lds	temp1, USART_B		;disable transmit empty interrupts
+	cbr	temp1, bm_ENABLE_INT_TX
+	sts	USART_B, temp1
+	out	SREG, temp0  
 #ifdef	RTS_ENABLE
-  RTS_INIT
+	RTS_INIT
 #endif
-  jmp_	DO_NEXT	
+	jmp_	DO_NEXT	
 

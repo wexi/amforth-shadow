fcb370ce786811bf6fe2360091587acc2fb6cd0e
Enoch <ixew@hotmail.com>
2015-06-11 14:38:09 -0400

    Raise trust to 1.0

--- core/words/env-forthversion.asm
+++ core/words/env-forthversion.asm
@@ -2,15 +2,15 @@
 ; Environment
 ; Forth version
 VE_ENV_FORTHVERSION:
-    .dw $ff07
-    .db "version",0
-    .dw VE_ENVHEAD
-    .set VE_ENVHEAD = VE_ENV_FORTHVERSION
+	.dw 	$ff07
+	.db 	"version",0
+	.dw 	VE_ENVHEAD
+	.set 	VE_ENVHEAD = VE_ENV_FORTHVERSION
 XT_ENV_FORTHVERSION:
-    .dw DO_COLON
+	.dw 	DO_COLON
 PFA_EN_FORTHVERSION:
-    .dw XT_DOSLITERAL
-    .dw 4
-    .db "V0.9"
-    .dw XT_EXIT
+	.dw 	XT_DOSLITERAL
+	.dw 	4
+	.db 	"V1.0"
+	.dw 	XT_EXIT
 

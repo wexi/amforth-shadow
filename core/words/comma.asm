a7e6c428d1f9e1813189ad92240c6f8483ea95a9
Enoch <ixew@hotmail.com>
2015-06-18 17:53:10 -0400

    compile faster please

--- core/words/comma.asm
+++ core/words/comma.asm
@@ -2,17 +2,14 @@
 ; Dictionary
 ; compile 16 bit into flash at DP
 VE_COMMA:
-    .dw $ff01
-    .db ',',0 ; ,
-    .dw VE_HEAD
-    .set VE_HEAD = VE_COMMA
+	.dw 	$ff01
+	.db 	',', 0
+	.dw 	VE_HEAD
+	.set 	VE_HEAD = VE_COMMA
 XT_COMMA:
-    .dw DO_COLON
+	.dw 	DO_COLON
 PFA_COMMA:
-    .dw XT_DP
-    .dw XT_STOREI
-    .dw XT_DP
-    .dw XT_1PLUS
-    .dw XT_DOTO
-    .dw PFA_DP
-    .dw XT_EXIT
+	.dw 	XT_DP		;see dp.asm
+	.dw 	XT_STOREI
+	.dw	XT_DPINC
+	.dw 	XT_EXIT
--- core/words/dp.asm
+++ core/words/dp.asm
@@ -2,13 +2,26 @@
 ; System Value
 ; address of the next free dictionary cell
 VE_DP:
-    .dw $ff02
-    .db "dp"
-    .dw VE_HEAD
-    .set VE_HEAD = VE_DP
+	.dw 	$ff02
+	.db 	"dp"
+	.dw 	VE_HEAD
+	.set 	VE_HEAD = VE_DP
 XT_DP:
-    .dw PFA_DOVALUE1
+	.dw 	PFA_DP1
 PFA_DP:
-    .dw RAM_DP
-    .dw XT_RDEFERFETCH
-    .dw XT_RDEFERSTORE
+	.dw 	RAM_DP
+	.dw 	XT_RDEFERFETCH
+	.dw 	XT_RDEFERSTORE
+;
+PFA_DP1:			;wish compile be faster
+	savetos
+	ldsw	TOS, RAM_DP
+	jmp_	DO_NEXT
+	
+XT_DPINC:			;"dp 1+ to dp" but faster
+	.dw	PFA_DPINC
+PFA_DPINC:
+	ldsw	Z, RAM_DP
+	adiw	zh:zl, 1
+	stsw	RAM_DP, Z
+	jmp_	DO_NEXT

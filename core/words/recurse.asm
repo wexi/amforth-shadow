aa783d8e3707a619e145608d191a8dfaaf55c760
Enoch <ixew@hotmail.com>
2015-11-24 17:56:02 -0500

    If RECURSE has a right to exist so has RECOURSE.

--- core/words/recurse.asm
+++ core/words/recurse.asm
@@ -13,3 +13,20 @@ PFA_RECURSE:
     .dw XT_FETCH
     .dw XT_COMMA
     .dw XT_EXIT
+	
+; ( -- ) 
+; Compiler
+; compile the XT literal of the word currently being defined into the dictionary
+VE_REWORD:
+    .dw $0008
+    .db "recourse"
+    .dw VE_HEAD
+    .set VE_HEAD = VE_REWORD
+XT_REWORD:
+    .dw DO_COLON
+PFA_REWORD:
+    .dw XT_COMPILE
+    .dw XT_DOLITERAL	
+    .dw XT_DOBRANCH
+    .dw PFA_RECURSE
+	
